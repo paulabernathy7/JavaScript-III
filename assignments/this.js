d1aed71f4d40d9433a05cbf39ad6d5d8cbc5c632
Paul Abernathy <paul.abernathy7@gmail.com>
2019-04-02 22:06:43 -0400

    this complete

--- assignments/this.js
+++ assignments/this.js
@@ -5,10 +5,10 @@
 
 * 2. Implicit binding Whenever a function is being called with a dot to the left, the object before the dot is this.
 
-* 3. In new binding you capaltilazing the first worrd in the function which creates a constructor function. The function creates a new object behind the scenes. The object has to be called using the new keyword
+* 3. In new binding you capaltilazing the first word in the function which creates a constructor function. The function creates a new object behind the scenes. The object has to be called using the new keyword
 
 
-* 4. In explict binding, you can use the call and, apply properties. We're able to call them explicitly using these properties. 
+* 4. In explict binding, you can use the call and, apply properties. We're able to call them explicitly using these properties using an array.
 *
 * write out a code example of each explanation above
 */
@@ -39,6 +39,35 @@ favHero.superHero('Batman');
 
 // code example for New Binding
 
+function MyFriends(friend) {
+    this.greeting = "Hey man";
+    this.friend = friend;
+    this.speak = function() {
+        console.log(this.greeting + this.friend);
+        console.log(this);
+    }; 
+
+}
+
+const Rudy = new MyFriends('Logan')
+const logan = new MyFriends('Rudy')
+
+logan.speak();
+logan.speak();
+
 // Principle 4
 
+
 // code example for Explicit Binding
+
+const players = ['Wade', 'Westbrook', 'LeBron']
+
+favorite = function(player1, player2, player3) {
+    console.log(`My favorite ${this.sport} players are ${player1}, ${player2}, and ${player3}`)
+};
+
+const paul = {
+    name: 'Paul',
+    sport: 'NBA'
+};
+favorite.call(paul, players[0], players[1], players[2]);
